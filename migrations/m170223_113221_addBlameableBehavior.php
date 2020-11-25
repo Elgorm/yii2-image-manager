7b3fc73ecf48d38012cb391e3fc92daa93173580
Elgorm <8758619+Elgorm@users.noreply.github.com>
2020-11-25 11:06:45 +0500

    Fix error

--- migrations/m170223_113221_addBlameableBehavior.php
+++ migrations/m170223_113221_addBlameableBehavior.php
@@ -7,7 +7,7 @@ class m170223_113221_addBlameableBehavior extends Migration
     public function up()
     {
         $this->addColumn('{{%image_manager}}', 'created_by', $this->integer()->unsigned()->null()->defaultValue(null));
-        $this->addColumn('{{%image_manager}}', 'created_by', $this->integer()->unsigned()->null()->defaultValue(null));
+        $this->addColumn('{{%image_manager}}', 'modified_by', $this->integer()->unsigned()->null()->defaultValue(null));
     }
 
     public function down()
